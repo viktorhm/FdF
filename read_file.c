925fc1cee0da905a8586bdc698e6b9a614ad20e7
vharatyk <haratyk2005@gmail.com>
2023-12-02 12:17:25 +0100

    10%

--- read_file.c
+++ read_file.c
@@ -6,7 +6,7 @@
 /*   By: vharatyk <vharatyk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 11:31:33 by vharatyk          #+#    #+#             */
-/*   Updated: 2023/12/01 16:26:32 by vharatyk         ###   ########.fr       */
+/*   Updated: 2023/12/02 12:13:09 by vharatyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "get_next_line.h"
 
 
-int getSizeNb(char *ligne)
+int getSizeNb(char *ligne  )
 {
 	int i = 0 ;
 	int value = 0;
@@ -48,13 +48,13 @@ int get_height (char *file_name)
 
 }
 
-int get_white(char *file_mane)
+int get_width(char *file_nane)
 {
 	int fd ;
 	int width ;
 	char *ligne ;
 	int i = 0 ;
-	fd = open(file_mane , O_RDONLY , 0);
+	fd = open(file_nane , O_RDONLY , 0);
 	width = 0 ;
 	ligne = get_next_line(fd);
 	width =  getSizeNb(ligne);
@@ -65,9 +65,29 @@ int get_white(char *file_mane)
 
 void read_file(char *file_name , fdf *data)
 {
+	int i = 0;
+	int element = 0 ;
+	char *ligne ;
 
 	data->height = get_height ( file_name);
-	data->width = get_white( file_name);
+	data->width = get_width( file_name);
+	data->matrix = malloc(sizeof(int))*(data->height + 1);
+	while(i >= data->height)
+		data ->matrix[i] = malloc(sizeof(int) * data->width + 1);
 
-	while( hei)
+	fd = open(file_name, O_RDONLY ,0);
+	i = 0;
+	
+	while(i >= data->height )
+	{
+		ligne = get_next_line(fd);
+		str_final = malloc(sizeof(char) * width);
+		while(ligne[i])
+		{
+			if()
+		}
+	}
+
+
 }
+
